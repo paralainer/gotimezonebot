772699eb50a52cad25a6c41c94a78442051b139f
stalov <stalov@atlassian.com>
2018-05-10 23:05:05 +1000

    moved to lambda

--- app/Bot.go
+++ app/Bot.go
@@ -3,6 +3,11 @@ package app
 import (
 	"log"
 	"gopkg.in/telegram-bot-api.v4"
+	"github.com/aws/aws-lambda-go/lambda"
+	"context"
+	"os"
+	"github.com/aws/aws-lambda-go/events"
+	"encoding/json"
 )
 
 type TgBot struct {
@@ -14,11 +19,11 @@ type TgBot struct {
 
 func StartBot(token string, locationService *MongoLocationsService, weather GetWeather) {
 	botApi, err := tgbotapi.NewBotAPI(token)
-	
+
 	if err != nil {
 		log.Panic(err)
 	}
-	
+
 	//botApi.Debug = true
 
 	bot := &TgBot{
@@ -31,20 +36,27 @@ func StartBot(token string, locationService *MongoLocationsService, weather GetW
 	bot.startBot()
 }
 
-func (bot *TgBot) startBot() {
+type Result struct {
+	MessageId int `json:"message_id"`
+	Status string `json:"status"`
+}
 
-	log.Printf("Authorized on account %s", bot.Api.Self.UserName)
+func toResponse(result Result, status int) events.APIGatewayProxyResponse {
+	bytes, _ := json.Marshal(result)
+	return events.APIGatewayProxyResponse { Body: string(bytes), StatusCode: status}
+}
+func (bot *TgBot) ListenForLambda() {
+	lambda.Start(func (context context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		token, ok := request.PathParameters["token"]
+		if !ok || token != os.Getenv("CALL_TOKEN") {
+			return toResponse(Result{ Status: "invalid_token"}, 403), nil
+		}
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.Api.GetUpdatesChan(u)
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	for update := range updates {
+		update := tgbotapi.Update{}
+		err := json.Unmarshal([]byte(request.Body), &update)
+		if err != nil {
+			return toResponse(Result{Status: "invalid_request"}, 400), nil
+		}
 		if update.Message != nil {
 			chat, ok := bot.chats[update.Message.Chat.ID]
 			if !ok {
@@ -52,8 +64,16 @@ func (bot *TgBot) startBot() {
 				bot.chats[update.Message.Chat.ID] = chat
 			}
 
-			go chat.ProcessMessage(update.Message)
+			chat.ProcessMessage(update.Message)
+			return toResponse(Result{Status: "ok", MessageId: update.Message.MessageID}, 200), nil
+		} else {
+			return toResponse(Result{Status: "not_a_message"}, 200), nil
 		}
-	}
+	})
+}
+
+func (bot *TgBot) startBot() {
+	log.Printf("Authorized on account %s", bot.Api.Self.UserName)
+	bot.ListenForLambda()
 }
 
